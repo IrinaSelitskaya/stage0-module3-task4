b05cfe1e089efa7cf699210376c1308db10c0f48
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-01-27 23:10:22 +0300

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-     int a=1;
-     int b=2;
+     int a;
+     int b;
      int x;
      x=(((9*(a*a))-(5*b))+2+a-7)*((a+b-(4*a*b))/2);
     }
